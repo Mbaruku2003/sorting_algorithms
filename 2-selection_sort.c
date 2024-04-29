1eb99ce2ecd7e587460d4cde74916138b40c1c06
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-29 06:40:08 -0700

    2-selection_sort.c updated

---
+++ 2-selection_sort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "sort.h"
+/**
+ * swap - swaps integer in an array
+ * @a: the value at a certain array pos
+ * @b:value at another array position
+ * Return: void
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+/**
+ * selection_sort - uses selectin sorting
+ * @array: array with elements
+ * @size: the size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t min_index;
+	size_t k;
+	size_t j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_index])
+			{
+				min_index = j;
+			}
+	swap(&array[i], &array[min_index]);
+	for (k = 0; k < size; k++)
+	{
+		printf("%d ", array[k]);
+	}
+	printf("\n");
+	}
+}
