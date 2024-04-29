e4288454f5cc5d6e8920d5448e1a303cd042e152
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-29 13:22:31 -0700

    0-bubble_sort.c updated

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,18 +2,18 @@
 #include "sort.h"
 /**
  * bubble_sort - uses bubble sort to sort
- * @arr: the array
+ * @array: the array
  * @size: location of the integer in array
  * Return: void
  */
-void bubble_sort(int *arr, size_t size)
+void bubble_sort(int *array, size_t size)
 {
 	size_t i;
 	size_t j;
 	size_t flag;
 	size_t temp;
 
-	if (arr == NULL || size == 0)
+	if (array == NULL || size == 0)
 	{
 		return;
 	}
@@ -22,13 +22,13 @@ void bubble_sort(int *arr, size_t size)
 		flag = 0;
 		for (j = 0; j < size - 1 - i; j++)
 		{
-			if (arr[j] > arr[j + 1])
+			if (array[j] > array[j + 1])
 			{
-				temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 
-				print_array(arr, size);
+				print_array(array, size);
 				flag = 1;
 			}
 		}
