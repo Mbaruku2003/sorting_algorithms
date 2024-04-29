b85b4171b59b050d82b4b4793d01d3b88b7fb529
Mbaruku2003 <leonardjembe21@gmail.com>
2024-04-29 12:16:53 -0700

    wow

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -31,7 +31,7 @@ void bubble_sort(int *arr, size_t size)
 		flag = 0;
 		for (j = 0; j < size - 1 - i; j++)
 		{
-			if ((arr[j] > arr[j + 1]) && size > 2)
+			if (arr[j] > arr[j + 1])
 			{
 				swap(&arr[j], &arr[j + 1]);
 				print_array(arr, size);
